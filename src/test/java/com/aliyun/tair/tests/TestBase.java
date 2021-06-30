b13fba3a32bd734fa33870c7acd7206fbb8ff3f5
bodong.ybd <bodong.ybd@alibaba-inc.com>
2021-06-30 11:35:38 +0800

    Add JedisRetryCommand class for simple retry

--- src/test/java/com/aliyun/tair/tests/TestBase.java
+++ src/test/java/com/aliyun/tair/tests/TestBase.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPool;
 
 public class TestBase {
     protected static final String HOST = "127.0.0.1";
@@ -13,15 +14,16 @@ public class TestBase {
     protected static final int CLUSTER_PORT = 30001;
 
     protected static Jedis jedis;
+    protected static JedisPool jedisPool;
     protected static JedisCluster jedisCluster;
 
     static {
         try {
-            jedis = new Jedis(HOST, PORT);
+            jedis = new Jedis(HOST, PORT, 60 * 1000); // timeout 60s
             if (!"PONG".equals(jedis.ping())) {
                 System.exit(-1);
             }
-
+            jedisPool = new JedisPool(HOST, PORT);
             Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
             jedisClusterNodes.add(new HostAndPort(HOST, CLUSTER_PORT));
             jedisCluster = new JedisCluster(jedisClusterNodes);
