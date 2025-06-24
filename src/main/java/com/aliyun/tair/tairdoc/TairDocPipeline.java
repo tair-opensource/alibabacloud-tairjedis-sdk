06d3419db6760e4c7e4b34ee7557064a6e1827d8
bodong.ybd <bodong.ybd@alibaba-inc.com>
2025-06-24 15:29:58 +0800

    TairDoc support JSON.MERGE command

--- src/main/java/com/aliyun/tair/tairdoc/TairDocPipeline.java
+++ src/main/java/com/aliyun/tair/tairdoc/TairDocPipeline.java
@@ -287,4 +287,14 @@ public class TairDocPipeline extends Pipeline {
         return appendCommand(new CommandObject<>(new CommandArguments(ModuleCommand.JSONARRTRIM).key(key).add(path)
             .add(start).add(stop), BuilderFactory.LONG));
     }
+
+    public Response<String> jsonMerge(final String key, final String path, final String value) {
+        getClient("").sendCommand(ModuleCommand.JSONMERGE, key, path, value);
+        return getResponse(BuilderFactory.STRING);
+    }
+
+    public Response<String> jsonMerge(final byte[] key, final byte[] path, final byte[] value) {
+        getClient("").sendCommand(ModuleCommand.JSONMERGE, key, path, value);
+        return getResponse(BuilderFactory.STRING);
+    }
 }
